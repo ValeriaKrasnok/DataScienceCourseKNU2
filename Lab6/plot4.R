c81e97b49468780b6ecb50347dd15b496d8f6009
ValeriaKrasnok <47188364+ValeriaKrasnok@users.noreply.github.com>
2019-12-15 00:22:58 +0200

    Create plot4.R

---
+++ Lab6/plot4.R
@@ -0,0 +1,15 @@
+data<-read.table("E:/Lab6RStudio/household_power_consumption.txt", sep = ";", header = T, na.strings = "?")
+##Reads in data from file then subsets data for specified dates
+lab <-subset(data, Date=="1/2/2007" | Date=="2/2/2007")
+##Filter data from 1/2/2007 and 2/2/2007
+lab$DateTime<-paste(lab$Date, lab$Time)
+lab$DateTime<-strptime(lab$DateTime, "%d/%m/%Y %H:%M:%S")
+lab$Date<-as.Date(lab$Date, "%d/%m/%Y")
+##Plot 4
+png("plot4.png", width=480, height=480)
+plot(lab$DateTime, lab$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(lab$DateTime, lab$Sub_metering_2, type = "l", col = "red")
+lines(lab$DateTime, lab$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1 ", "Sub_metering_2 ", "Sub_metering_3 "), bty = "n")
+plot(lab$DateTime, lab$Global_reactive_power, type = "l", xlab = "datetime",  ylab = "Global_reactive_power")
+dev.off()
