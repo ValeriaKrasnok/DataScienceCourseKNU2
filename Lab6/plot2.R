a50b51e568cd9c52768484852731dfaa92e91011
ValeriaKrasnok <47188364+ValeriaKrasnok@users.noreply.github.com>
2019-12-15 00:19:28 +0200

    Create plot2.R

---
+++ Lab6/plot2.R
@@ -0,0 +1,11 @@
+data<-read.table("E:/Lab6RStudio/household_power_consumption.txt", sep = ";", header = T, na.strings = "?")
+##Reads in data from file then subsets data for specified dates
+lab <-subset(data, Date=="1/2/2007" | Date=="2/2/2007")
+##Filter data from 1/2/2007 and 2/2/2007
+lab$DateTime<-paste(lab$Date, lab$Time)
+lab$DateTime<-strptime(lab$DateTime, "%d/%m/%Y %H:%M:%S")
+lab$Date<-as.Date(lab$Date, "%d/%m/%Y")
+##Plot 2
+png("plot2.png", width=480, height=480)
+plot(lab$DateTime, lab$Global_active_power, type = "l", xlab = "",  ylab = "Global Active Power (kilowatts)")
+dev.off()
