3dc4a0ba8bfdd784395084147f9d193ac875672c
ValeriaKrasnok <47188364+ValeriaKrasnok@users.noreply.github.com>
2019-12-15 03:24:11 +0200

    Update plot4.R

--- Lab7/plot4.R
+++ Lab7/plot4.R
@@ -5,4 +5,4 @@ join1<- merge(NEI, SCC, by.x ='SCC', by.y='SCC' )
 join2 <- subset(join1, grepl("(Comb)(.)+(Coal)", join1$Short.Name))
 answer<- rename(aggregate(x=join2[,'Emissions'], by=list(join2[,'year']), FUN = sum), Year = Group.1,  Emissions.in.tonns=x)
 plot(answer, type='b', xlab='Year', main = 'Emissions from coal combustion-related sources in USA', ylab='Sum of emissions(tonns)')
-##PM2.5 emissions from 1999 to 2008 have decreased
+##PM2.5 emissions from 1999 to 2008 had decreased
