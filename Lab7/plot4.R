46b299b782f814a3c465205de20791f93ea14239
ValeriaKrasnok <47188364+ValeriaKrasnok@users.noreply.github.com>
2019-12-15 02:59:11 +0200

    Create plot4.R

---
+++ Lab7/plot4.R
@@ -0,0 +1,8 @@
+##Reading data
+NEI<-readRDS("E:/exdata/summarySCC_PM25.rds")
+SCC<-readRDS("E:/exdata/Source_Classification_Code.rds")
+join1<- merge(NEI, SCC, by.x ='SCC', by.y='SCC' )
+join2 <- subset(join1, grepl("(Comb)(.)+(Coal)", join1$Short.Name))
+answer<- rename(aggregate(x=join2[,'Emissions'], by=list(join2[,'year']), FUN = sum), Year = Group.1,  Emissions.in.tonns=x)
+plot(answer, type='b', xlab='Year', main = 'Emissions from coal combustion-related sources in USA', ylab='Sum of emissions(tonns)')
+##PM2.5 emissions from 1999 to 2008 have decreased
